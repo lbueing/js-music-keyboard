26a87cf47ed7f4568bb9c4b67a67cbc4de32ccac
Leah Bueing <lbueing@gmail.com>
2016-11-21 15:09:00 -0800

    click and keyboard working

--- noise.js
+++ noise.js
@@ -1,3 +1,32 @@
 $(document).ready( function() {
   // your code here
+
+  $('.instrument').on('click', 'button', function(event) {
+    var keys = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+    for (var key of keys) {
+      if ($(this).html() == key) {
+        var note = document.getElementById(key + "Audio");
+        note.load();
+        note.play();
+      }
+    }
+ });
+
+
+
+$('body').keydown(function(event){
+  var keys = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+  for (var key of keys) {
+    if (event.key == key) {
+      var note = document.getElementById(key + "Audio");
+      note.load();
+      note.play();
+    }
+  }
 });
+
+});
+ //   if (event.key == ) {
+ //     alert("hey you got an a!");
+ //   }
+ // });
